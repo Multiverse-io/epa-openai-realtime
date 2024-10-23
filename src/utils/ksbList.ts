c84e073e491b7f4a55ecd243b5d4a3e10df51f9e
Lara Thompson <larathompson>
2024-10-23 14:57:17 +0100

    small changes to text

--- src/utils/ksbList.ts
+++ src/utils/ksbList.ts
@@ -3,14 +3,14 @@ import { KSBListItem, practiceStatus } from '../components/EPAPracticeList';
 const ksbList: KSBListItem[] = [
   {
     id: 'K5',
-    name: 'Structured and Unstructured data',
+    name: 'KSB5 - Structured and Unstructured data',
     definition:
       'Explains the differences between Structured and Unstructured data.',
     status: practiceStatus.notStarted,
   },
   {
     id: 'B6',
-    name: 'Resilience',
+    name: 'KSB6 - Resilience',
     definition:
       'Demonstrates resilience by viewing obstacles as challenges and learning from failure.',
     status: practiceStatus.notStarted,
